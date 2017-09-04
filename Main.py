1e62df0acf5102c65dddc77af3207b6064b0dc47
Simon-Bogyo Szabolcs <simon.szabolcs10@gmail.com>
2017-09-05 01:13:45 +0300

    Extraxt ROI mean color

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import sys
 import cv2
-
+import numpy
 
 def load_image():
     """Loads an image from disk, returns it as a single instance"""
@@ -21,10 +21,9 @@ def pixelate_image(image):
     height, width = image.shape[:2]  # We know it should be a fixed res
     blurred_image = cv2.medianBlur(image, 61)
 
-
     """ 16:9 aspect ratio means we can make (16:9) * n size blocks """
-    width_increment = 16
-    height_increment = 9
+    width_increment = 16 * 2
+    height_increment = 9 * 2
 
     # Init matrix
     roi_matrix = [[0 for x in range(width / width_increment)] for y in range(height / height_increment)]
@@ -34,9 +33,15 @@ def pixelate_image(image):
         for y in range(0, height - height_increment, height_increment):
             roi_matrix[x/width_increment][y/height_increment] = blurred_image[int(y):int(y + height_increment), int(x):int(x + width_increment)]
 
-    im_crop = roi_matrix[2][1]
-
-    show_image(im_crop)
+    # See http://miriamposner.com/classes/medimages/3-use-opencv-to-find-the-average-color-of-an-image/
+    # TODO: move to process_matrix
+    test_roi = roi_matrix[12][15]
+    show_image(test_roi)
+    average_color_row = numpy.average(test_roi, axis=0)
+    average_color = numpy.average(average_color_row, axis=0)
+    average_color = numpy.uint8(average_color)
+    average_color_img = numpy.array([[average_color] * 160] * 90, numpy.uint8)
+    show_image(average_color_img)
 
     # Debug only
     for i in range(0, width, width_increment):
